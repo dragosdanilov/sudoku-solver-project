b205bbd80e8570609830d717211ce19fa28feab5
Dragos Danilov <dragosdanilov266@gmail.com>
2023-03-06 04:23:37 -0500

    Add function to recursively solve Sudoku using a backtracking algorithm

--- main.py
+++ main.py
@@ -15,6 +15,27 @@ def is_valid_move(grid, row, col, number):
                 return False
     return True
 
+def solve(grid, row, col):
+    if col == 9:
+        if row == 8:
+            return True
+        row += 1
+        col = 0
+
+    if grid[row][col] > 0:
+        return solve(grid, row, col + 1)
+    
+    for num in range(1, 10):
+        if is_valid_move(grid, row, col, num):
+            grid[row][col] = num
+
+            if solve(grid, row, col + 1):
+                return True
+            
+        grid[row][col] = 0
+
+    return False
+
 grid = [[2, 5, 0, 0, 8, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [6, 0, 9, 1, 0, 0, 0, 4, 2],
