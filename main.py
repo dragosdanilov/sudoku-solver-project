b99ef150d6c32676828d1656bcc8dfb47f8b3508
Dragos Danilov <dragosdanilov266@gmail.com>
2023-03-05 23:58:19 -0500

    Implement helper function for checking move validity in a 3x3 field

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+def is_valid_move(grid, row, col, number):
+    for x in range(9):
+        if grid[row][x] == number:
+            return False
+        
+    for y in range(9):
+        if grid[col][y] == number:
+            return False
+        
+    corner_row = row - row % 3
+    corner_col = col - col % 3
+    for x in range(3):
+        for y in range(3):
+            if grid[corner_row + x][corner_col + y] == number:
+                return False
+    return True
+
 grid = [[2, 5, 0, 0, 8, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [6, 0, 9, 1, 0, 0, 0, 4, 2],
